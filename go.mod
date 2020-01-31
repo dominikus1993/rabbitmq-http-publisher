95b241ed00d86673ca25baeb18db2e33ad69792f
dominikus1993 <dominikus1910@gmail.com>
2020-01-31 19:32:43 +0000

    add logging

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module rabbitmq-http-publisher
 go 1.13
 
 require (
+	github.com/gin-gonic/gin v1.5.0
 	github.com/sirupsen/logrus v1.4.2
 	github.com/streadway/amqp v0.0.0-20200108173154-1c71cc93ed71
 )
--- main.go
+++ main.go
@@ -1,6 +1,22 @@
 package main 
 
+import (
+	"github.com/gin-gonic/gin"
+	log "github.com/sirupsen/logrus"
+	"rabbitmq-http-publisher/app/infrastructure/ginlogrus"
+)
+
 
 func main() {
-	println("Test")
+	log.SetFormatter(&log.JSONFormatter{})
+	logg := log.New()
+	r := gin.New()
+	r.Use(ginlogrus.Logger(logg))
+	r.Use(gin.Recovery())
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run("0.0.0.0:9000") // listen and serve on 0.0.0.0:8080
 }
